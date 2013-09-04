601aa19977616a01ef3ed05e9ce6e4c3d663e888
GBT3101 <gbt3101@gmail.com>
2013-09-04 14:40:21 +0300

    backup deleted

--- גיבוי/src/Minesweeper/Constants.java
+++
@@ -1,9 +0,0 @@
-package Minesweeper;
-
-public class Constants {
-	
-	// This class is only so the highscores will not be deleted on a new game
-	
-	public static final HighScores highScores = new HighScores();
-
-}
